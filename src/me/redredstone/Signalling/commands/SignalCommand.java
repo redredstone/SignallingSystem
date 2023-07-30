5a748d437368017363630374d1309ea4d55dc6df
redredstone <138786369+redredstone@users.noreply.github.com>
2023-07-30 20:34:44 +0200

    Update SignalCommand.java

--- src/me/redredstone/Signalling/commands/SignalCommand.java
+++ src/me/redredstone/Signalling/commands/SignalCommand.java
@@ -38,37 +38,70 @@ public class SignalCommand implements CommandExecutor {
 						Location l = plr.getLocation();
 						String item = args[1];
 						if (item.toLowerCase().equals("signal")) {
-							//Location
-							int plrLoc = plr.getLocation().getBlockY();
-							
-							Location origin = plr.getEyeLocation();
-							Vector dir = origin.getDirection();
-							Location center = origin.clone().add(dir).add(0,plrLoc + 1,0);
-							
-							
-							//Rotation
-							Location rotated = origin.clone();
-							rotated.setPitch(0);
-							rotated.setYaw(origin.getYaw());
-							Vector rot = rotated.getDirection();
-							
-							//Building a signal
-							Location blockLocation = center.clone().add(rot).add(0,-95,0);
-							
-							blockLocation.getBlock().setType(Material.OAK_FENCE);
-							blockLocation.add(0,1,0).getBlock().setType(Material.BLACK_CONCRETE);
-							blockLocation.add(0,1,0).getBlock().setType(Material.BLACK_CONCRETE);
-							
-							//Item frames
-							Entity frame1 = (Entity) plr.getWorld().spawnEntity(blockLocation.add(dir), EntityType.ITEM_FRAME);
-							Entity frame2 = (Entity) plr.getWorld().spawnEntity(blockLocation.add(0,-1,0), EntityType.ITEM_FRAME);
-							
-							plr.sendMessage(ChatColor.GREEN + "[Signalling] You have created a signal at " + l.getBlockX() + " " + l.getBlockY() + " " + l.getBlockZ());
-							
+							try {
+								//Location
+								int plrLoc = plr.getLocation().getBlockY();
+								Location origin = plr.getEyeLocation();
+								
+								int remove = plrLoc - origin.getBlockY();
+								
+								Vector dir = origin.getDirection();
+								Location center = origin.clone().add(dir).add(0,remove,0);
+								
+								
+								//Rotation
+								Location rotated = origin.clone();
+								rotated.setPitch(0);
+								rotated.setYaw(origin.getYaw());
+								Vector rot = rotated.getDirection();
+								
+								//Building a signal
+								Location blockLocation = center.clone().add(rot);
+								
+								blockLocation.getBlock().setType(Material.OAK_FENCE);
+								blockLocation.add(0,1,0).getBlock().setType(Material.BLACK_CONCRETE);
+								blockLocation.add(0,1,0).getBlock().setType(Material.BLACK_CONCRETE);
+								
+								//Item frames
+								ItemFrame frame1 = (ItemFrame) plr.getWorld().spawnEntity(blockLocation.add(dir), EntityType.ITEM_FRAME);
+								ItemFrame frame2 = (ItemFrame) plr.getWorld().spawnEntity(blockLocation.add(0,-1,0), EntityType.ITEM_FRAME);
+								
+								//Item frame properties
+								ItemStack red = new ItemStack(Material.RED_CONCRETE);
+								ItemStack black = new ItemStack(Material.BLACK_CONCRETE);
+								
+								//Set to Invulnerable
+								frame1.setInvulnerable(true);
+								frame2.setInvulnerable(true);
+								
+								//Set to invisible
+								frame1.setVisible(false);
+								frame2.setVisible(false);
+								
+								//Set the aspect of the signal
+								frame1.setItem(black);
+								frame2.setItem(red);
+								
+								//Rotate the frame
+								frame1.setRotation(Rotation.CLOCKWISE_45);
+								frame2.setRotation(Rotation.CLOCKWISE_45);
+								
+								plr.sendMessage(ChatColor.GREEN + "[Signalling] You have created a signal at " + l.getBlockX() + " " + l.getBlockY() + " " + l.getBlockZ());
+								
+							}
+							catch(Exception e) {
+								plr.sendMessage(ChatColor.RED + "[AUTOMATED MESSAGE: SIGNALLING] An internal error has been detected! Please report this to the Server Technician.");
+								return false;
+							}
 						} else if (item.toLowerCase().equals("sensor")) {
-							plr.sendMessage(ChatColor.GREEN + "[Signalling] You have created a sensor at " + l.getBlockX() + " " + l.getBlockY() + " " + l.getBlockZ());
-							Location blockLocation = plr.getLocation().add(0,-1,0);
-							blockLocation.getBlock().setType(Material.BLUE_CONCRETE);
+							try {
+								plr.sendMessage(ChatColor.GREEN + "[Signalling] You have created a sensor at " + l.getBlockX() + " " + l.getBlockY() + " " + l.getBlockZ());
+								Location blockLocation = plr.getLocation().add(0,-1,0);
+								blockLocation.getBlock().setType(Material.BLUE_CONCRETE);
+							}
+							catch(Exception e) {
+								plr.sendMessage(ChatColor.RED + "[AUTOMATED MESSAGE: SIGNALLING] An internal error has been detected! Please report this to the Server Technician");
+							}
 						} else {
 							plr.sendMessage(ChatColor.RED + "[Signalling] You are invalid");
 						}
