9939a9010b5c5793b868906f0b6fd77234fff97a
redredstone <138786369+redredstone@users.noreply.github.com>
2023-07-30 16:47:31 +0200

    Update SignalCommand.java

--- src/me/redredstone/Signalling/commands/SignalCommand.java
+++ src/me/redredstone/Signalling/commands/SignalCommand.java
@@ -39,29 +39,38 @@ public class SignalCommand implements CommandExecutor {
 						String item = args[1];
 						if (item.toLowerCase().equals("signal")) {
 							//Location
+							int plrLoc = plr.getLocation().getBlockY();
+							
 							Location origin = plr.getEyeLocation();
 							Vector dir = origin.getDirection();
-							Location center = origin.clone().add(dir);
+							Location center = origin.clone().add(dir).add(0,plrLoc + 1,0);
+							
 							
 							//Rotation
 							Location rotated = origin.clone();
 							rotated.setPitch(0);
-							rotated.setYaw(origin.getYaw() - 90);
+							rotated.setYaw(origin.getYaw());
 							Vector rot = rotated.getDirection();
 							
 							//Building a signal
-							Location blockLocation = center.clone().add(rot);
+							Location blockLocation = center.clone().add(rot).add(0,-95,0);
+							
 							blockLocation.getBlock().setType(Material.OAK_FENCE);
 							blockLocation.add(0,1,0).getBlock().setType(Material.BLACK_CONCRETE);
 							blockLocation.add(0,1,0).getBlock().setType(Material.BLACK_CONCRETE);
 							
+							//Item frames
+							Entity frame1 = (Entity) plr.getWorld().spawnEntity(blockLocation.add(dir), EntityType.ITEM_FRAME);
+							Entity frame2 = (Entity) plr.getWorld().spawnEntity(blockLocation.add(0,-1,0), EntityType.ITEM_FRAME);
+							
 							plr.sendMessage(ChatColor.GREEN + "[Signalling] You have created a signal at " + l.getBlockX() + " " + l.getBlockY() + " " + l.getBlockZ());
+							
 						} else if (item.toLowerCase().equals("sensor")) {
 							plr.sendMessage(ChatColor.GREEN + "[Signalling] You have created a sensor at " + l.getBlockX() + " " + l.getBlockY() + " " + l.getBlockZ());
 							Location blockLocation = plr.getLocation().add(0,-1,0);
 							blockLocation.getBlock().setType(Material.BLUE_CONCRETE);
 						} else {
-							plr.sendMessage(ChatColor.RED + "[Signalling] This is not a valid argument.");
+							plr.sendMessage(ChatColor.RED + "[Signalling] You are invalid");
 						}
 						
 					}
