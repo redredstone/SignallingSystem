b9a5e9034a6da39aa3ef489dab428e915117c8eb
redredstone <138786369+redredstone@users.noreply.github.com>
2023-08-03 13:54:43 +0200

    Updated Main.java

--- src/me/redredstone/Signalling/Main.java
+++ src/me/redredstone/Signalling/Main.java
@@ -1,5 +1,10 @@
 package me.redredstone.Signalling;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import org.bukkit.Location;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.redredstone.Signalling.commands.SignalCommand;
@@ -11,10 +16,89 @@ public class Main extends JavaPlugin {
 		new SignalCommand(this);
 		
 		//Load Data
+		File signallingData = new File(getDataFolder(), "Data.yml");
+		
+		if (!(signallingData.exists())) {
+			saveResource("Data.yml", false);
+		}
+	    
 	}
 	
 	public void onDisable() {
 		//Save data
+		try {
+			File signallingData = new File(getDataFolder(), "Data.yml");
+			YamlConfiguration yamlData = YamlConfiguration.loadConfiguration(signallingData);
+			
+			yamlData.save(signallingData);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	//Signals
+	
+	public void getSignal(String signalID) {
+		
+	}
+	
+	public void addSignal(String signalID, Location signalLocation) {
+		YamlConfiguration yaml = new YamlConfiguration();
+		File signallingData = new File(getDataFolder(), "Data.yml");
+		
+		yaml.createSection("signallingData");
+		yaml.createSection("signallingData.signals");
+		
+		ArrayList<Object> signalInfo = new ArrayList<Object>();
+		
+		signalInfo.add(signalLocation.getBlockX());
+		signalInfo.add(signalLocation.getBlockY());
+		signalInfo.add(signalLocation.getBlockZ());
+		signalInfo.add(signalLocation.getPitch());
+		signalInfo.add(signalLocation.getYaw());
+		
+		yaml.set("signallingData.signals." + signalID, signalInfo);
+		try {
+			yaml.save(signallingData);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//Sensors
+	
+	public void addSensor(String sensorID, Location sensorLocaiton) {
+		YamlConfiguration yaml = new YamlConfiguration();
+		File signallingData = new File(getDataFolder(), "Data.yml");
+		
+		yaml.createSection("signallingData");
+		yaml.createSection("signallingData.signals");
+		
+		ArrayList<Object> signalInfo = new ArrayList<Object>();
+		
+		signalInfo.add(sensorLocaiton.getBlockX());
+		signalInfo.add(sensorLocaiton.getBlockY());
+		signalInfo.add(sensorLocaiton.getBlockZ());
+		signalInfo.add(sensorLocaiton.getPitch());
+		signalInfo.add(sensorLocaiton.getYaw());
+		
+		yaml.set("signallingData.signals." + sensorID, signalInfo);
+		try {
+			yaml.save(signallingData);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setProperty(String type) {
+		if (type.toLowerCase().equals("signal")) {
+			
+		} else if (type.toLowerCase().equals("")) {
+			
+		}
 	}
 }
 
